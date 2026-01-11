603575e21342c35dd1d00f1def19a6351bed9c6a
kiyohken2000 <www.broadwell.com@gmail.com>
2026-01-11 17:58:35 +0900

    master

---
+++ apps/mobile/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { View } from 'react-native'
+import { Provider } from 'react-redux'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import store from 'utils/store'
+import 'utils/ignore'
+import { UserContextProvider } from './contexts/UserContext'
+
+// assets
+import { imageAssets } from 'theme/images'
+import { fontAssets } from 'theme/fonts'
+import Router from './routes'
+
+export default function App() {
+  // state
+  const [didLoad, setDidLoad] = useState(false)
+
+  // handler
+  const handleLoadAssets = async () => {
+    // assets preloading
+    await Promise.all([...imageAssets, ...fontAssets])
+    setDidLoad(true)
+  }
+
+  // lifecycle
+  useEffect(() => {
+    handleLoadAssets()
+  }, [])
+
+  // rendering
+  if (!didLoad) return <View />
+  return (
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <UserContextProvider>
+          <Router />
+        </UserContextProvider>
+      </Provider>
+    </SafeAreaProvider>
+  )
+}
